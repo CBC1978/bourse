bdc3dc52c0147b0d9b092c1fe8caa606f1fa2324
CBC1978 <132070698+CBC1978@users.noreply.github.com>
2023-11-04 01:55:18 +0000

    Update AdminSettingsController.php

--- app/Http/Controllers/Admin/parameter/AdminSettingsController.php
+++ app/Http/Controllers/Admin/parameter/AdminSettingsController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\admin\parameter\AdminSettingsController;
+namespace App\Http\Controllers\admin\parameter;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
